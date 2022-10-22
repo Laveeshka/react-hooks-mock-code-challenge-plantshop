9196d41b793a174c344926039679bb953139b7c1
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-10-22 22:46:49 +1100

    render all plants

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  //lift state to this level
+  const [plants, setPlants] = useState([]);
+
+  //fetch all plants on mount
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then(res => res.json())
+      .then(plantData => setPlants(plantData))
+      .catch(err => console.warn(err.message))
+  }, [])
+
   return (
     <main>
       <NewPlantForm />
       <Search />
-      <PlantList />
+      <PlantList plants={plants}/>
     </main>
   );
 }
